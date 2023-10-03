ea1daae5b07ce659ce8f0adcabcf42477de09ea9
Sameer Kongath <zoomixinfo@gmail.com>
2023-10-03 23:29:38 +0530

    Get and Post Added

--- apidata/views.py
+++ apidata/views.py
@@ -2,7 +2,17 @@ from django.shortcuts import render
 from . models import Address
 from django.http import JsonResponse
 from . serializer import AddressSerilaizer
+from rest_framework.decorators import api_view
+
+@api_view(["GET","POST"])
 def index(request):
-    address=Address.objects.all()
-    serilaizer=AddressSerilaizer(address, many=True)
-    return JsonResponse(serilaizer.data, safe=False)
+    if request.method=="GET":
+        address=Address.objects.all()
+        serilaizer=AddressSerilaizer(address, many=True)
+        return JsonResponse(serilaizer.data, safe=False)
+    if request.method=="POST":
+        serilaizer=AddressSerilaizer(data=request.data)
+        if serilaizer.is_valid():
+            serilaizer.save()
+            return JsonResponse(serilaizer.data, safe=False)
+    return JsonResponse()
