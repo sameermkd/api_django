0254b5c4546b3ec4dbca3d1cf3263d829cd49796
Sameer Kongath <zoomixinfo@gmail.com>
2023-10-03 23:51:49 +0530

    All functionadded and Complete

--- apidata/views.py
+++ apidata/views.py
@@ -16,3 +16,21 @@ def index(request):
             serilaizer.save()
             return JsonResponse(serilaizer.data, safe=False)
     return JsonResponse()
+@api_view(['GET','PUT','DELETE','PUT'])
+def data(request,id):
+    try:
+        address=Address.objects.get(pk=id)
+    except Address.DoesNotExist:
+        return JsonResponse({'Error':'Address not found'}, status=404)
+    if request.method=='GET':
+        serilaizer=AddressSerilaizer(address)
+        return JsonResponse(serilaizer.data)
+    elif request.method=='PUT':
+        serilaizer=AddressSerilaizer(data=request.data)
+        if serilaizer.is_valid():
+            serilaizer.save()
+            return JsonResponse(serilaizer.data)
+    elif request.method=='DELETE':
+        address.delete()
+        return JsonResponse({'Deleted':'Address Deleted'}, status=204)
+    return JsonResponse()
